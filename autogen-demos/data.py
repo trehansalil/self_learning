cfc7a5876a8c7a4d53b13b7a17419619dd9a1ba5
Salil Trehan <trehansalil@yahoo.in>
2025-06-22 21:23:41 +0530

    Add data processing script for agent-based interaction
    - Introduced a new Python script `data.py` that implements an agent-based system for processing Excel data.
    - Integrated `AssistantAgent` and `CodeExecutorAgent` to facilitate code execution based on user queries about the dataset.
    - Utilized `DockerCommandLineCodeExecutor` for executing Python code in a Docker environment.
    - Implemented a round-robin group chat mechanism to manage interactions between agents and handle task results.

---
+++ autogen-demos/data.py
@@ -0,0 +1,71 @@
+import code
+from autogen_agentchat.agents import AssistantAgent, CodeExecutorAgent
+from autogen_agentchat.base import TaskResult
+from autogen_agentchat.messages import TextMessage
+from autogen_core import code_executor
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_ext.code_executors.docker import DockerCommandLineCodeExecutor
+from autogen_agentchat.teams import RoundRobinGroupChat
+
+from autogen_agentchat.conditions import TextMentionTermination
+
+import asyncio
+
+from src.constants import EURI_API_BASE_URL, EURI_API_KEY, EURI_MODEL_NAME_GPT_4_1_NANO
+
+async def main():
+    model = OpenAIChatCompletionClient(
+        model=EURI_MODEL_NAME_GPT_4_1_NANO,
+        api_key=EURI_API_KEY,
+        base_url=EURI_API_BASE_URL
+    )
+
+
+    developer = AssistantAgent(
+        name='Developer',
+        model_client=model,
+        system_message=("You're a developer agent. You will be given a excel data file"
+        "and a question about it. You can develop python code to answer the question"
+        "You should always begin with you plan to answer your question then you"
+        "should write the code to answer the question"
+        "You should always write the code in code blocks with language with Python language only"
+        "if you need several code blocks, write them one at a time"
+        "You will be working with a code executor agent to execute the code. If the code"
+        "is executed successfully, you can continue."
+        "If a library is required, you should install it using pip install <library_name>"
+        "Once you have the code execution results, you should provide the final answer and"
+        "After that you should exactly say TERMINATE to terminate the conversation at the end"
+        )
+    )
+
+    docker = DockerCommandLineCodeExecutor(
+        image='custom-python-3.10',
+        work_dir="temp",
+    )
+
+    await docker.start()
+
+    executor = CodeExecutorAgent(
+        name="CodeExecutor",
+        code_executor=docker,
+    )
+
+    # Create a group chat with the agents
+    team = RoundRobinGroupChat(
+        participants=[developer, executor],
+        termination_condition=TextMentionTermination(text="TERMINATE"),
+        max_turns=30,
+    )
+
+    task = "My dataset is Active Worker Per Occupation Level.xlsx. What are the columns in the dataset?"
+
+    async for msg in team.run_stream(task = task):
+        if isinstance(msg, TextMessage):
+            print(f"{msg.source}: {msg.content}")
+        elif isinstance(msg, TaskResult):
+            print(f"Stop Reason: {msg.stop_reason}")
+
+    await docker.stop()
+
+if __name__ == "__main__":
+    asyncio.run(main())
